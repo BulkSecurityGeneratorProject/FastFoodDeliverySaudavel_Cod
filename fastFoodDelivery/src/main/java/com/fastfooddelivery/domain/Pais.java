c67eb19a52ff6e48a359561e8b9e3641b208afb8
andersonsilvaa <andersonsilva0692@gmail.com>
2017-11-11 18:54:03 -0200

    implementa alteracao na classe pais e cartao

--- fastFoodDelivery/src/main/java/com/fastfooddelivery/domain/Pais.java
+++ fastFoodDelivery/src/main/java/com/fastfooddelivery/domain/Pais.java
@@ -66,7 +66,6 @@ public class Pais implements Serializable {
     public void setCartaos(Set<Cartao> cartaos) {
         this.cartaos = cartaos;
     }
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
 
     @Override
     public boolean equals(Object o) {
